b43e82066e055c4f0071c9c998ff3660b8236732
Umesh Kumar <umeshkumarkhanna@hotmail.com>
2015-10-11 04:24:31 -0400

    added picasso

--- app/src/main/java/ca/umeshkhanna/gitme/UserProfileActivity.java
+++ app/src/main/java/ca/umeshkhanna/gitme/UserProfileActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class UserProfileActivity extends AppCompatActivity {
 
@@ -12,6 +15,8 @@ public class UserProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        ImageView userImage = (ImageView)findViewById(R.id.userImage);
+
         Bundle userBundle = getIntent().getExtras();
         String name = userBundle.getString("name");
         String email = userBundle.getString("email");
@@ -19,7 +24,8 @@ public class UserProfileActivity extends AppCompatActivity {
         String location = userBundle.getString("location");
         int followers = userBundle.getInt("followers");
         int following = userBundle.getInt("following");
-  
+
+        Picasso.with(this).load(avatar_url).into(userImage);
 
 
 
