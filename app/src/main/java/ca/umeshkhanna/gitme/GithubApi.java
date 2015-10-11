075bcbf968be99f8b80fe9f656ce531228f553ef
Umesh Kumar <umeshkumarkhanna@hotmail.com>
2015-10-11 03:30:31 -0400

    updated get

--- app/src/main/java/ca/umeshkhanna/gitme/GithubApi.java
+++ app/src/main/java/ca/umeshkhanna/gitme/GithubApi.java
@@ -1,6 +1,9 @@
 package ca.umeshkhanna.gitme;
 
+import com.squareup.okhttp.OkHttpClient;
+
 import retrofit.Callback;
+import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
 import retrofit.http.GET;
@@ -11,18 +14,15 @@ import retrofit.http.Path;
  */
 
 public class GithubApi {
-
     private static final String BASE_URL = "https://api.github.com";
     private static final String ACCEPT_HEADER_NAME = "Accept";
     private static final String ACCEPT_HEADER_VALUE = "application/vnd.github.v3+json";
 
     private static GitHubService gitHubService = null;
 
-    private GithubApi(){
+    private GithubApi() {
 
     }
-
-
 
 
     public static GitHubService getInstance() {
@@ -30,18 +30,25 @@ public class GithubApi {
             RestAdapter restAdapter = new RestAdapter.Builder()
                     .setLogLevel(RestAdapter.LogLevel.FULL)
                     .setClient(new OkClient(new OkHttpClient()))
-                    .setRequestInterceptor(request -> {
-                        request.addHeader(ACCEPT_HEADER_NAME, ACCEPT_HEADER_VALUE);
-                    })
-                    .setEndpoint(BASE_URL)
-                    .build();
 
-            gitHubService = restAdapter.create(GitHubService.class);
-        }
-        return gitHubService;
+
+            .setRequestInterceptor(new RequestInterceptor() {
+                @Override
+                public void intercept(RequestFacade request) {
+                    request.addHeader(ACCEPT_HEADER_NAME, ACCEPT_HEADER_VALUE);
+                }
+                );
+
+                .setEndpoint(BASE_URL)
+
+                .build();
+
+                gitHubService=restAdapter.create(GitHubService.class);
+            }
+            return gitHubService;
     }
 
-    interface GitHubService{
+    interface GitHubService {
         @GET("/users/{user}")
         void getUser(@Path("user") String username, Callback<User> callback);
     }
