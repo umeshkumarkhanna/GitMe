b8c3f9d120a4b02ee66b987d8a9c4a474038285a
Umesh Kumar <umeshkumarkhanna@hotmail.com>
2015-10-11 03:23:49 -0400

    connected api end to code

--- app/src/main/java/ca/umeshkhanna/gitme/GithubApi.java
+++ app/src/main/java/ca/umeshkhanna/gitme/GithubApi.java
@@ -1,5 +1,8 @@
 package ca.umeshkhanna.gitme;
 
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.client.OkClient;
 import retrofit.http.GET;
 import retrofit.http.Path;
 
@@ -7,15 +10,36 @@ import retrofit.http.Path;
  * Created by umeshkhanna on 15-10-11.
  */
 public class GithubApi {
-    interface GithubService{
+    interface GitHubService{
         @GET("/users/{user}")
         void getUser(@Path("user") String username, Callback<User> callback);
     }
 
-    private static GithubService githubService = null;
+    private static GitHubService gitHubService = null;
 
     private GithubApi(){
-        
+
+    }
+
+
+    private static final String BASE_URL = "https://api.github.com";
+    private static final String ACCEPT_HEADER_NAME = "Accept";
+    private static final String ACCEPT_HEADER_VALUE = "application/vnd.github.v3+json";
+
+    public static GitHubService getInstance() {
+        if (gitHubService == null) {
+            RestAdapter restAdapter = new RestAdapter.Builder()
+                    .setLogLevel(RestAdapter.LogLevel.FULL)
+                    .setClient(new OkClient(new OkHttpClient()))
+                    .setRequestInterceptor(request -> {
+                        request.addHeader(ACCEPT_HEADER_NAME, ACCEPT_HEADER_VALUE);
+                    })
+                    .setEndpoint(BASE_URL)
+                    .build();
+
+            gitHubService = restAdapter.create(GitHubService.class);
+        }
+        return gitHubService;
     }
 
 }
